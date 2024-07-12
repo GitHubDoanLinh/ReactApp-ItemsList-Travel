b996d453e06e3e00efee24a31913bbe953aa14dd
Doan Huu Linh <doanlinh1510@gmail.com>
2024-07-12 18:43:39 +0700

    commit2 Rendering the Items List

--- src/App.js
+++ src/App.js
@@ -1,32 +1,65 @@
 import "./index.css";
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Chager", quantity: 2, packed: false },
+];
 
 export default function App() {
-  return <div className="app">
-    <Logo/>
-    <Form/>
-    <PackingList/>
-    <Stats/>
-  </div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 function Logo() {
-  return <h1>🧳 A Nice Trip 🏝️</h1>
+  return <h1>🧳 A Nice Trip 🏝️</h1>;
 }
 function Form() {
-  return(
-    <div className="add-form">
+  return (
+    <Form className="add-form">
       <h3>What do you need for your trip 😍?</h3>
-    </div>
-  )
+      <select>
+        <option value={1}>1</option>
+        <option value={2}>2</option>
+        <option value={3}>3</option>
+        <option value={4}>4</option>
+        <option value={5}>5</option>
+      </select>
+      <input type="text" placeholder="Item..."/>
+    </Form>
+  );
 }
 function PackingList() {
-  return(
-    <div className="list">LIST</div>
-  )
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+function Item({ item }) {
+  return (
+    <li>
+      <span
+        style={item.packed === true ? { textDecoration: "line-through" } : {}}
+      >
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 function Stats() {
-  return(
+  return (
     <footer className="stats">
       <em>🧳You have X items for your list.</em>
     </footer>
-  )
+  );
 }
